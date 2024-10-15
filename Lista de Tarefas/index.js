bb2f11f9221a16d3503e6cf6ee26e750ef30e44b
ackindev <ackinlino123@gmail.com>
2024-10-15 13:55:20 -0300

    Add: Estilização Lista de tarefas e funcionalidades

--- Lista de Tarefas/index.js
+++ Lista de Tarefas/index.js
@@ -4,6 +4,79 @@ const todoListUl = document.querySelector("#todo-list");
 
 let tasks = []; //array para as tarefas
 
+function renderTaskOnHTML(taskTitle, done = false) {
+  const li = document.createElement("li");
+
+  const input = document.createElement("input"); // <input>
+  input.setAttribute("type", "checkbox"); // <input type="checkbox"/>
+  input.addEventListener("change", (event) => {
+    const liToToggle = event.target.parentElement;
+
+    const spanToToggle = liToToggle.querySelector("span");
+
+    const done = event.target.checked;
+    if (done) {
+      spanToToggle.style.textDecoration = "line-through";
+    } else {
+      spanToToggle.style.textDecoration = "none";
+    }
+
+    tasks = tasks.map((t) => {
+      if (t.title === spanToToggle.textContent) {
+        return {
+          title: t.title,
+          done: !t.done,
+        };
+      }
+      return t;
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  input.checked = done;
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  if (done) {
+    span.style.textDecoration = "line-through";
+  }
+
+  const btn = document.createElement("button");
+  btn.textContent = "Remover";
+  btn.addEventListener("click", (event) => {
+    const liToRemove = event.target.parentElement;
+
+    const titleToRemove = liToRemove.querySelector("span").textContent; // Tarefa 1
+
+    tasks = tasks.filter((t) => t.title !== titleToRemove);
+
+    todoListUl.removeChild(liToRemove);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(btn);
+
+  todoListUl.appendChild(li);
+}
+
+window.onload = () => {
+  const tasksOnLocalStorage = localStorage.getItem("tasks");
+  console.log(tasksOnLocalStorage);
+
+  if (!tasksOnLocalStorage) return;
+
+  tasks = JSON.parse(tasksOnLocalStorage);
+
+  tasks.forEach((t) => {
+    renderTaskOnHTML(t.title, t.done);
+  });
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // Evita o comportamento padrão de revarregar a página ao submeter o formulário
 
@@ -19,30 +92,11 @@ form.addEventListener("submit", (event) => {
     title: taskTitle,
     done: false,
   });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
   console.log(tasks);
 
   // Adicionando a nova tarefa no HTML
-  const li = document.createElement("li");
-
-  const input = document.createElement("input"); // <input>
-  input.setAttribute("type", "checkbox"); // <input type="checkbox"/>
-
-  const span = document.createElement("span");
-  span.textContent = taskTitle;
-
-  const btn = document.createElement("button");
-  btn.textContent = "Remover";
-  btn.addEventListener("click", (event) => {
-    console.log(event.target.parentElement);
-    todoListUl.removeChild(event.target.parentElement);
-  });
-
-  li.appendChild(input);
-  li.appendChild(span);
-  li.appendChild(btn);
-
-  todoListUl.appendChild(li);
-
+  renderTaskOnHTML(taskTitle);
   taskTitleInput.value = "";
 });
